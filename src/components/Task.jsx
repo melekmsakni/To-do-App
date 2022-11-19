07eb3a219e91bf13b1a05939d12c7a8adc7c8fee
melek msakni <65375259+melekmsakni@users.noreply.github.com>
2022-11-19 13:19:47 +0100

    adding done button

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import { FaTimes } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaTimes, FaCheck } from "react-icons/fa";
 
-function Task({ content, deletep,remin }) {
+function Task({ content, deletep, remin }) {
+  const [done, setDone] = useState(false);
+  let taskDone = () => {
+    setDone(!done);
+  };
+  let style = "task";
+  style += done ? " lineThrough" : "";
+  style += content.reminder ? ` task-reminder` : ``;
+
   return (
-    <div
-      className={content.reminder ? "task task-reminder" : "task"}
-      onDoubleClick={() => remin(content.id)}
-    >
-      <h4>
-        {content.text}{" "}
-        <FaTimes style={{ color: "red" }} onClick={() => deletep(content.id)} />
-      </h4>
+    <div className={style} onDoubleClick={() => remin(content.id)}>
+      <div className="txtcontent">
+        <h4> {content.text} </h4>
+        <div className="icons">
+          <FaCheck
+            className="tick"
+            style={{ color: "green" }}
+            onClick={taskDone}
+          />
+          <FaTimes
+            style={{ color: "red" }}
+            onClick={() => deletep(content.id)}
+          />
+        </div>
+      </div>
       <p>{content.day}</p>
     </div>
   );
